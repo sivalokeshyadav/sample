4c35537af76881b198617872f33a9b4def756272
Sivalokesh Yadav <222sivalokeshyadav@gmail.com>
2023-07-04 19:06:36 +0530

    commit

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,12 @@
+
+import { Component } from "react";
+
+class LoginForm extends Component{
+    render(){
+        return(
+            <div>LoginForm</div>
+        )
+    }
+}
+
+export default LoginForm
